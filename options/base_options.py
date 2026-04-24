0a94ada6d67ba1c4fa6be6b72558482c3d6c3a63
stef-tim <s.b.timmermans@student.rug.nl>
2026-04-24 20:34:28 +0200

    fix no_html for eval

--- options/base_options.py
+++ options/base_options.py
@@ -51,6 +51,7 @@ class BaseOptions:
         parser.add_argument("--load_iter", type=int, default="0", help="which iteration to load? if load_iter > 0, the code will load models by iter_[load_iter]; otherwise, the code will load models by [epoch]")
         parser.add_argument("--verbose", action="store_true", help="if specified, print more debugging information")
         parser.add_argument("--suffix", default="", type=str, help="customized suffix: opt.name = opt.name + suffix: e.g., {model}_{netG}_size{load_size}")
+        parser.add_argument('--no_html', action='store_true', help='do not save intermediate training results to [opt.checkpoints_dir]/[opt.name]/web/')
         # wandb parameters
         parser.add_argument("--use_wandb", action="store_true", help="if specified, then init wandb logging")
         parser.add_argument("--wandb_project_name", type=str, default="pix2pix-uint16", help="specify wandb project name")
